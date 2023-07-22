80591d15b219fdf7e37b5bdfeac0d5406430d781
yufei <yuflying@163.com>
2023-07-23 00:01:20 +0800

    feat: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,29 +1,57 @@
 import { defineConfig } from 'vitepress'
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Hello World",
-  description: "博大可以稀释忧愁",
+  title: "gkit",
+  description: "Go 服务框架",
   base: '/gkit-doc/',
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: '主页', link: '/' },
+      { text: '文档', link: '/intro/' },
+      {
+        text: '组件',
+        items: [
+          { text: '命令行工具', link: 'https://codeup.aliyun.com/qimao/leo/leo/blob/master/cmd/README.md' },
+          { text: 'Lib 工具包', link: 'https://codeup.aliyun.com/qimao/leo/lib' },
+        ]
+      },
+      { text: '参考', link: '/reference/api-examples' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: '简介',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: '简介', link: '/intro/' },
+          { text: '设计理念', link: '/intro/design' },
+          { text: '常见问题', link: '/intro/faq' },
+          { text: '项目结构', link: '/intro/design' }
+        ]
+      },
+      {
+        text: '快速开始',
+        items: [
+          { text: '项目初始化', link: '/getting-started/start' },
+          { text: '代码示例', link: '/getting-started/examples' }
+        ]
+      },
+      {
+        text: '组件',
+        items: [
+          { text: '项目初始化', link: '/component/' }
+        ]
+      },
+      {
+        text: '开源社区',
+        items: [
+          { text: '贡献指南', link: '/community/contribution' },
+          { text: '文档维护', link: '/community/dcoumentation' }
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/meiigo/gkit' }
     ]
   }
 })
--- docs/index.md
+++ docs/index.md
@@ -1,25 +1,24 @@
 ---
-# https://vitepress.dev/reference/default-theme-home-page
 layout: home
 
 hero:
-  name: "Hello World"
-  text: "博大可以稀释忧愁"
-  tagline: My great project tagline
+  name: "gkit"
+  text: "Go 服务框架"
+  tagline: 基于 Go 语言的服务框架，脚手架工具，用于快速生成项目模板。
   actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
     - theme: alt
-      text: API Examples
-      link: /api-examples
+      text: 介绍
+      link: /intro/design
+    - theme: brand
+      text: 开始
+      link: /getting-started/start
 
 features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+  - title: 简单
+    details: 不断追求
+  - title: 自由
+    details: 框架只提供接口设计，具体实现由模块子包来实现，方便团队自定义实现。
+  - title: 灵活
+    details: 整个框架类似工具包，可以按需选择自己需要的功能
 ---
 
